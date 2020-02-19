7a6e99796166daad42e993d755f53dc85a40886e
Peilonrayz <peilonrayz@gmail.com>
2020-02-19 16:45:48 +0000

    Stop using check-manifest

--- noxfile.py
+++ noxfile.py
@@ -50,7 +50,7 @@ def hint(session):
 @nox.session
 def lint(session):
     session.install("nox", "vox", "check-manifest")
-    session.run("check-manifest")
+    # session.run("check-manifest")
     session.run("nox", "-r", "-f", "noxfile-lint.py", "--", *session.posargs)
 
 
